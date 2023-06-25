1f67aa45e41ca980e45c33ab160c198d5b5fa819
MilovanMilovanov <ninapredninazad@gmail.com>
2023-06-26 00:45:30 +0300

    Delete dist directory

--- dist/adjus.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=adjus.js.map
--- dist/globals.js
+++
@@ -1,9 +0,0 @@
-export const colors = {
-    appBgColor: "#800020",
-    cardFronColor: "white",
-    cardBackColor: "lightgreen",
-    btnColor: "#0ea17a",
-    btnHoverColor: "#0ed17a",
-    btnLabelColor: "white",
-};
-//# sourceMappingURL=globals.js.map
